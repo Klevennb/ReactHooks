186572c1f5c2b98c5b513d402dd1290fb356707c
Matt Kleven <klevennb115@gmail.com>
2019-06-11 22:40:48 -0500

    moved files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,9 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <button>Counter</button>
-      </header>
+      <h1>Hi</h1>
     </div>
   );
 }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,32 +1,14 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
 
 
-import "./styles.css";
-
-function App() {
-    const [count, setCount] = useState(0);
-
-    return (
-        <div className="App">
-            <h1>Hello CodeSandbox</h1>
-            <h2>You clicked {count} times!</h2>
-
-            <button onClick={() => setCount(count - 1)}>Decrement</button>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
-        </div>
-    );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
